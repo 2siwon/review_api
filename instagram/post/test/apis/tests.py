49b5dd6049c222074d283d86323534074d8e2540
2siwon <lee89640245@gmail.com>
2017-11-09 15:53:31 +0900

    post_list get요청에서 author가 None인 Post는 표시되지 않는지 확인하는 테스트 작성

--- instagram/post/test/apis/tests.py
+++ instagram/post/test/apis/tests.py
@@ -18,6 +18,14 @@ class PostListViewTest(APILiveServerTestCase):
     URL_API_POST_LIST = '/api/post/'
     VIEW_CLASS = PostList
 
+    @staticmethod
+    def create_user(username='dummy'):
+        return User.objects.create_user(username=username, age=0)
+
+    @staticmethod
+    def create_post(author=None):
+        return Post.objects.create(author=author, photo=File(io.BytesIO()))
+
     def test_post_list_url_name_reverse(self):
         url = reverse(self.URL_API_POST_LIST_NAME)
         self.assertEqual(url, self.URL_API_POST_LIST)
@@ -35,10 +43,10 @@ class PostListViewTest(APILiveServerTestCase):
             self.VIEW_CLASS)
 
     def test_get_post_list(self):
-        user = User.objects.create_user(username='dummy', age=0)
-        # 0이상 20 이하의 임의의 숫자 지정
+        user = self.create_user()
+        # 0이상 20이하의 임의의 숫자 지정
         num = randint(0, 20)
-        # num 개수만큼 Post 생성
+        # num개수만큼 Post생성, author를 지정해줌
         for i in range(num):
             Post.objects.create(
                 author=user,
@@ -53,4 +61,31 @@ class PostListViewTest(APILiveServerTestCase):
         # objects.count결과가 num과 같은지 확인
         self.assertEqual(Post.objects.count(), num)
         # response로 돌아온 JSON리스트의 길이가 num과 같은지 확인
-        self.assertEqual(len(response.data), 0)
+        self.assertEqual(len(response.data), num)
+
+        # response로 돌아온 객체들이 각각 pk, author, photo, created_at
+        for i in range(num):
+            cur_post_data = response.data[i]
+            self.assertIn('pk', cur_post_data)
+            self.assertIn('author', cur_post_data)
+            self.assertIn('photo', cur_post_data)
+            self.assertIn('created_at', cur_post_data)
+
+    def test_get_post_list_exclude_author_is_none(self):
+        """
+        author가 None인 Post가 PostList get요청에서 제외되는지 테스트
+        :return:
+        """
+        user = self.create_user()
+        num_author_none_posts = randint(0, 10)
+        num_posts = randint(11, 20)
+
+        for i in range(randint(0, 10)):
+            self.create_post()
+        for i in range(num_posts):
+            self.create_post(author=user)
+
+        response = self.client.get(self.URL_API_POST_LIST)
+        # author가 없는 Post개수는 response에 포함되지 않는지 확인
+        self.assertEqual(len(response.data), num_posts)
+
